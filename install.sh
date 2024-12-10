7673611e6135b3ad5b15824085d3b2df86c549d8
Shiva Pundir <36535845+ceevaaa@users.noreply.github.com>
2024-12-10 16:19:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ install_docker_ubuntu() {
     echo "[INFO] Installing Docker on Ubuntu..."
     sudo apt-get update -y
     sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update -y
     sudo apt-get install -y docker-ce
