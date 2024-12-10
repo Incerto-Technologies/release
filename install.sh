a76b72caa8b62c2413f64f2524e5daf1c89d7527
Shiva Pundir <36535845+ceevaaa@users.noreply.github.com>
2024-12-10 22:41:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,17 +34,18 @@ install_docker_ubuntu() {
     sudo apt-get install -y docker-ce
     sudo systemctl enable docker
     sudo systemctl start docker
+    echo "[SUCCESS] Docker installed successfully on UBUNTU."
 }
 
 # Function to install Docker on RHEL
 install_docker_rhel() {
     echo "[INFO] Installing Docker on RHEL..."
-    sudo yum update -y
-    sudo yum install -y yum-utils
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    sudo yum install -y docker-ce docker-ce-cli containerd.io
-    sudo systemctl enable docker
-    sudo systemctl start docker
+    sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine podman runc
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl enable --now docker
+    echo "[SUCCESS] Docker installed successfully on RHEL."
 }
 
 # After installation: Set up Docker group and permissions
