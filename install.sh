bb6f7ff61a5805ab131580fc46537b181872f038
Shiva Pundir <36535845+ceevaaa@users.noreply.github.com>
2024-12-10 22:01:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,36 +2,33 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Constants
-AWS_REGION="ap-south-1"  # Replace with your AWS region
 ECR_URL="public.ecr.aws/t9w7u8l8/incerto"
 IMAGE_NAME="collector"
 IMAGE_TAG="latest"
-BACKEND_URL="https://your-backend-url.com/hostid"
 CONTAINER_NAME="incerto-collector"
-COLLECTOR_CONFIG_URL="https://raw.githubusercontent.com/Incerto-Technologies/collector/refs/heads/main/config.yaml"  # URL to download the config file
+
+# To get `HOST_ID` for a unique host
+BACKEND_ENDPOINT="http://localhost:8080"
+
+# `config.yaml`
+COLLECTOR_CONFIG_URL="https://raw.githubusercontent.com/Incerto-Technologies/collector/refs/heads/main/config.yaml"  
 COLLECTOR_CONFIG_FILE="config.yaml"
 COLLECTOR_CONFIG_BACKUP_FILE="config.yaml.bak"
+
+# `.env`
+COLLECTOR_ENV_URL="https://raw.githubusercontent.com/Incerto-Technologies/collector/refs/heads/main/.env"
+COLLECTOR_ENV_FILE=".env"
+COLLECTOR_ENV_BACKUP_FILE=".env.bak"
 
 # Function to install Docker on Ubuntu
 install_docker_ubuntu() {
     echo "[INFO] Installing Docker on Ubuntu..."
     sudo apt-get update -y
     sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-
-    # Remove existing keyring file if it exists to avoid prompts
     if [ -f /usr/share/keyrings/docker-archive-keyring.gpg ]; then
-        echo "[INFO] Removing existing Docker GPG keyring file..."
         sudo rm -f /usr/share/keyrings/docker-archive-keyring.gpg
-        echo "[SUCCESS] Removed existing Docker GPG keyring file."
     fi
-
-    # Download and save the GPG key
-    echo "[INFO] Downloading Docker GPG key..."
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "[SUCCESS] Docker GPG key downloaded and saved."
-
-    # Add Docker repository
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update -y
     sudo apt-get install -y docker-ce
@@ -59,10 +56,10 @@ configure_docker_post_install() {
     echo "[SUCCESS] Docker group configured. You can now run Docker commands without sudo."
 }
 
-if command -v docker &> /dev/null && docker --version &> /dev/null; then
-    echo "[INFO] Docker is already installed. Version: $(docker --version)"
-else
-    echo "[INFO] Docker is not installed. Proceeding with installation..."
+# Check and install Docker
+if ! command -v docker &> /dev/null
+then
+    echo "[INFO] Docker not found. Starting installation process..."
     if [ -f /etc/os-release ]; then
         . /etc/os-release
         case "$ID" in
@@ -77,10 +74,12 @@ else
         echo "[ERROR] OS detection failed. Unable to proceed."
         exit 1
     fi
+
     # Perform post-installation steps
     configure_docker_post_install
+else
+    echo "[INFO] Docker is already installed. Skipping installation."
 fi
-
 
 # Pull the latest image from public ECR
 echo "[INFO] Pulling the latest Docker image from Public ECR..."
@@ -111,8 +110,34 @@ if [ $? -ne 0 ]; then
 fi
 echo "[SUCCESS] Configuration file downloaded successfully."
 
+# Download .env file and handle backup if it already exists
+echo "[INFO] Checking for an existing configuration file..."
+if [ -f "$COLLECTOR_ENV_FILE" ]; then
+    echo "[INFO] Configuration file found. Creating a backup..."
+    mv "$COLLECTOR_ENV_FILE" "$COLLECTOR_ENV_BACKUP_FILE"
+    echo "[SUCCESS] Backup created as $COLLECTOR_ENV_BACKUP_FILE."
+fi
+
+echo "[INFO] Downloading the latest configuration file..."
+curl -fsSL -o "$COLLECTOR_ENV_FILE" "$COLLECTOR_ENV_URL"
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to download the configuration file. Exiting."
+    exit 1
+fi
+echo "[SUCCESS] Configuration file downloaded successfully."
+
+# Fetch hostID from backend
+# echo "[INFO] Fetching hostID from the backend..."
+# HOST_ID=$(curl -sf $BACKEND_ENDPOINT)
+# if [ -z "$HOST_ID" ]; then
+#     echo "[ERROR] Failed to fetch hostID. Exiting."
+#     exit 1
+# fi
+HOST_ID="0000000000"
+echo "[INFO] HostID fetched: $HOST_ID"
+
 # Run the new container
 echo "[INFO] Starting a new container with the latest image..."
-docker run -d --name incerto-collector -v $(pwd)/config.yaml:/config.yaml -e HOST_ID="$HOST_ID" $ECR_URL/$IMAGE_NAME:$IMAGE_TAG 
+docker run --name incerto-collector --env-file ./.env -v $(pwd)/config.yaml:/config.yaml $ECR_URL/$IMAGE_NAME:$IMAGE_TAG 
 
 echo "[SUCCESS] Container is up and running."
