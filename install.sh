67814c79b1ebcebea99bc40e10c1a57c83573633
Shiva Pundir <36535845+ceevaaa@users.noreply.github.com>
2024-12-10 23:36:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,11 +84,11 @@ configure_docker_post_install() {
 # Function to verify Docker installation and functionality
 is_docker_installed() {
     if ! command -v docker &> /dev/null; then
-        echo "[ERROR] Docker is not installed or not in PATH."
+        echo "[INFO] Docker is not installed or not in PATH."
         return 1
     fi
     if ! docker --version &> /dev/null; then
-        echo "[ERROR] Docker command exists but is not functional."
+        echo "[INFO] Docker command exists but is not functional."
         return 1
     fi
     echo "[INFO] Docker is installed and functional. Version: $(docker --version)"
