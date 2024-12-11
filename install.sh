7f2ff5f4e5a890bdc8a1e271c189cb4e267e016f
Shiva Pundir <36535845+ceevaaa@users.noreply.github.com>
2024-12-11 13:47:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,7 +78,6 @@ configure_docker_post_install() {
     echo "[INFO] Configuring Docker group and permissions..."
     sudo groupadd docker || true  # Create the Docker group if it doesn't exist
     sudo usermod -aG docker $USER  # Add the current user to the Docker group
-    newgrp docker
     echo "[SUCCESS] Docker group configured. Please logout and log back in."
 }
 
