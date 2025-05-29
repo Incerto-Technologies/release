d60657208ba0f9df99793a44f0bdc8a185f4e199
Shiva Pundir <36535845+ceevaaa@users.noreply.github.com>
2025-05-29 15:45:24 +0530

    added y flag in docker installation

--- tool/offline/bundle.sh
+++ tool/offline/bundle.sh
@@ -271,7 +271,7 @@ install_docker_rhel() {
             printf "[INFO] Detected Red Hat. Installing Docker for RedHat/CentOS ...\n"
             sudo dnf -y install dnf-plugins-core
             sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-            sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+            sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
             sudo systemctl enable --now docker
             printf "[SUCCESS] Docker installed on RedHat/CentOS.\n"
             return
